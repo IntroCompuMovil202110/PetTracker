28074658a6c7739bd5f903f89d6fe843284cc80e
JohanGarcia01 <ferneypachon01@outlook.es>
2021-05-26 12:21:28 -0500

    notifications

---
+++ app/src/main/java/com/example/pettracker/Controller/NotificationJobIntentService.java
@@ -0,0 +1,134 @@
+package com.example.pettracker.Controller;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.JobIntentService;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.pettracker.Model.Message;
+import com.example.pettracker.R;
+import com.example.pettracker.View.ChatListActivity;
+import com.example.pettracker.View.PersonalChatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NotificationJobIntentService extends JobIntentService {
+    private static final int JOB_ID = 12;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference availables, myRef;
+    public static String CHANNEL_ID = "Notificaciones";
+    public static final String PATH_AVAILABLE="messages/";
+    private Map<String, String> mensajes;
+
+    public static void enqueueWork(Context context, Intent intent) {
+        enqueueWork(context, NotificationJobIntentService.class, JOB_ID, intent);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        availables = firebaseDatabase.getReference(PATH_AVAILABLE+firebaseAuth.getUid());
+        mensajes = new HashMap<>();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onHandleWork(@NonNull Intent intent){
+        availables.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot d: dataSnapshot.getChildren())
+                {
+                    for(DataSnapshot de: d.getChildren())
+                    {
+                        Message mensaje = de.getValue(Message.class);
+                        mensajes.put(de.getKey(), mensaje.getEmisorKey());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        availables.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(firebaseAuth.getCurrentUser() != null)
+                {
+                    for(DataSnapshot d: dataSnapshot.getChildren())
+                    {
+                        for(DataSnapshot de: d.getChildren())
+                        {
+                            Message mensaje = de.getValue(Message.class);
+                            if(!mensajes.containsKey(de.getKey())){
+                                if(!mensaje.getEmisorKey().equalsIgnoreCase(firebaseAuth.getUid())){
+                                    BuildNotification(d.getKey());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void BuildNotification(String id)
+    {
+        myRef = FirebaseDatabase.getInstance().getReference("users");
+        myRef.child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(NotificationJobIntentService.this, CHANNEL_ID);
+                mBuilder.setSmallIcon(R.drawable.bell);
+                mBuilder.setContentTitle("Nuevo mensaje");
+                mBuilder.setContentText(snapshot.child("nombre").getValue().toString()+ " te acaba de enviar un mensaje. Haz click verlo");
+                mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                Intent intent = new Intent(NotificationJobIntentService.this, PersonalChatActivity.class);
+                intent.putExtra("keyReceptor", id);
+                intent.putExtra("receptorName", snapshot.child("nombre").getValue().toString()+" "+snapshot.child("apellido").getValue().toString());
+                intent.putExtra("profileURL", snapshot.child("fotoPerfilURL").getValue().toString());
+
+
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(NotificationJobIntentService.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                mBuilder.setContentIntent(pendingIntent);
+                mBuilder.setAutoCancel(true); //Remueve la notificación cuando se toca*/
+
+                int notificationId = 001;
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(NotificationJobIntentService.this);
+                notificationManager.notify(notificationId, mBuilder.build());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/pettracker/View/HomePageActivity.java
+++ app/src/main/java/com/example/pettracker/View/HomePageActivity.java
@@ -8,13 +8,17 @@ import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.pettracker.Controller.NotificationJobIntentService;
 import com.example.pettracker.Controller.PermissionsManagerPT;
 import com.example.pettracker.Model.Product;
 import com.example.pettracker.R;
@@ -34,6 +38,9 @@ public class HomePageActivity extends AppCompatActivity implements NavigationVie
     CardView gatos;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+
+
+    public static String CHANNEL_ID = "Notificaciones";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +67,9 @@ public class HomePageActivity extends AppCompatActivity implements NavigationVie
 
         perros = findViewById(R.id.perros);
         gatos = findViewById(R.id.gatos);
+
+        init();
+        createNotificationChannel();
 
         comida.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -203,5 +213,28 @@ public class HomePageActivity extends AppCompatActivity implements NavigationVie
 
     public void SearchKeyWord(View v) {
         startActivity(new Intent(this, SearchResultsMainActivity.class));
+    }
+
+    private void init() {
+        Intent intent = new Intent(HomePageActivity.this, NotificationJobIntentService.class);
+        NotificationJobIntentService.enqueueWork(this, intent);
+        System.out.println("Profesor....");
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Notificaciones";
+            String description = "mostrar el cambio de estado de un usuario";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            //IMPORTANCE_MAX MUESTRA LA NOTIFICACIÓN ANIMADA
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
